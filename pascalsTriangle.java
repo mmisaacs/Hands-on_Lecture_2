e3a2ae4151bcdf877825cc91a6d565680d5123b6
Maddie Isaacs <misaacs@cpp.edu>
2025-02-02 18:22:38 -0800

    .

---
+++ pascalsTriangle.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class pascalsTriangle{
+    public List<List<Integer>> generate(int numRows){
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        return triangle;
+    }
+
+    public void main(String[] args){
+        System.out.println(generate(5));
+    }
+}
