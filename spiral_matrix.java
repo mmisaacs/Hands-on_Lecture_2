fb3271c622503ae67a696119c4e71ceb16489c71
Maddie Isaacs <misaacs@cpp.edu>
2025-02-02 21:20:49 -0800

    base parsing,, still some errors in results

--- spiral_matrix.java
+++ spiral_matrix.java
@@ -3,7 +3,7 @@ import java.util.List;
 //import java.util.Scanner;
 
 public class spiral_matrix {
-    public List<Integer> spiralOrder(int[][] matrix){
+    public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
 
         //setting boundaries
@@ -11,6 +11,37 @@ public class spiral_matrix {
         int rowEnd = matrix.length - 1;
         int colStart = 0;
         int colEnd = matrix[0].length - 1;
+
+        //parse through matrix, checks if counter and boundaries are equal(matrix is full parsed through)
+        while(rowStart <= rowEnd && colStart <= colEnd){
+            for (int i = colStart; i <= colEnd; i++) {
+                result.add(matrix[rowStart][i]);
+            }
+
+            colStart++;
+
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result.add(matrix[i][colEnd]);
+            }
+
+            colEnd--;
+
+            //move bottom row backwards
+            if(rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    result.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+
+            //move bottom to top on left column
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result.add(matrix[i][colStart]);
+                }
+                colStart++;
+            }
+        }
 
         return result;
     }
